313646d1e38dbd3cea90df6c2400d39a36d676ca
Cranial Break Productions <67295983+A-Random-Gamer-Nerd@users.noreply.github.com>
2026-09-22 16:46:45 -0500

    Initial commit

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float appleDropDelay = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
